d22252078abf0676887e4da18014ba4734e735f8
DESKTOP-ITRVK8L\thinh <thinhtm99@gmail.com>
2020-11-29 10:09:40 +0700

    Thêm Login controller Tạo view index, blog Tạo view login, register Liên kết các link của view

---
+++ Web/Controllers/LoginController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(FormCollection fc)
+        {
+            return RedirectToAction("Index", "My");
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(FormCollection fc)
+        {
+            return RedirectToAction("Index", "My");
+        }
+    }
+}
